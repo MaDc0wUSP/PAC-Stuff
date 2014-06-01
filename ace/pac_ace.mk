e48a60fb2d08820392163a6f5ea57abb0ee8a810
MaDc0wUSP <MaDc0w@undergroundz.org>
2014-06-01 13:54:49 +0200

    Desire HD shizzle

---
+++ ace/pac_ace.mk
@@ -0,0 +1,18 @@
+# Check for target product
+ifeq (pac_ace,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xhdpi
+# Add bluefa1con bootanim
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/720x1280/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/htc/ace/cm.mk)
+
+PRODUCT_NAME := pac_ace
+
+endif
