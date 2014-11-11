dfe44189d76ae23dda3aa452801f010233f9dabe
MaDc0wUSP <MaDc0w@undergroundz.org>
2014-11-11 20:33:42 +0100

    Reboot

--- ace/pac_ace.mk
+++ ace/pac_ace.mk
@@ -1,11 +1,13 @@
 # Check for target product
 ifeq (pac_ace,$(TARGET_PRODUCT))
 
-# OVERLAY_TARGET adds overlay asset source
-OVERLAY_TARGET := pa_xhdpi
-# Add bluefa1con bootanim
+# PAC boot logo
 PRODUCT_COPY_FILES += \
-    vendor/pac/prebuilt/720x1280/bootanimation.zip:system/media/bootanimation.zip
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/logo.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/480x800/bootanimation.zip:system/media/bootanimation.zip
 
 # include PAC common configuration
 include vendor/pac/config/pac_common.mk
